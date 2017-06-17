5a872fa77d61ae1e7b75acffcb5b97039d51e685
jcaleman <jcaleman.castillo@gmail.com>
2017-06-17 13:51:20 -0500

    js de Estudiosocioeconomico

---
+++ ICERP/Scripts/pages/estudiosocioeconomico.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+
+    //ICERP_Core.llamarAjax("EstudioSocioeconomico.aspx/obtenerTiposConsultorios", null, "crearControlTiposConsultorios");
+
+    //ICERP_Core.llamarAjax("EstudioSocioeconomico.aspx/obtenerConsultorios", null, "crearTablaConsultorios");
+
+    $("#btnGuardarEstudioSE").click(function (ev) {
+        var isValid = ICERP_Core.validarFormulario('registrarEstudioSE');
+        if (!isValid) {
+            ICERP_Core.reiniciarValidación('registrarEstudioSE');
+            ev.preventDefault();
+            return;
+        }
+        guardarEstudioSE();
+    });
+
+    
+});
+function guardarEstudioSE() {
+    //var nombre = $("#txtNombreConsultorio").val();
+    //var planta = ($("#sltPlanta").val() == "1");
+    //var activo = $("#chkActivo").prop('checked');
+
+    //var consultorio = new Object();
+    //consultorio.Nombre = nombre;
+    //consultorio.Planta = planta;
+    //consultorio.Activo = activo;
+    //consultorio.ConsultoriosTipos = [];
+
+    //$(".tiposConsultorios").each(function () {
+    //    var checked = $(this).prop('checked');
+    //    if (checked) {
+    //        var idTipo = Number($(this).val());
+    //        var consultorioTipo = new Object();
+    //        consultorioTipo.IdTipo = idTipo;
+    //        consultorio.ConsultoriosTipos.push(consultorioTipo);
+    //    }
+    //});
+
+    //ICERP_Core.bloquearPantalla();
+    //ICERP_Core.llamarAjax("Consultorios.aspx/guardarConsultorio", "{ 'consultorio': " + JSON.stringify(consultorio) + "}", "consultorioGuardado");
+}
