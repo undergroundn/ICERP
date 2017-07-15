e24dfef986226110ac8ec51f5913549161c2b652
jcaleman <jcaleman.castillo@gmail.com>
2017-07-15 09:51:26 -0500

    cambios pendientes de estudio

--- ICERP/Scripts/pages/estudiosocioeconomico.js
+++ ICERP/Scripts/pages/estudiosocioeconomico.js
@@ -17,9 +17,46 @@
     
 });
 function guardarEstudioSE() {
-    //var nombre = $("#txtNombreConsultorio").val();
-    //var planta = ($("#sltPlanta").val() == "1");
-    //var activo = $("#chkActivo").prop('checked');
+    //usuario encuestador
+    var nombreusuario = $("#nombreusuario").val();//poner autocomplete de usuarios registrados
+    var fecha = $("#fecha").val();//poner datepicker con fecha actual
+    //datos generales
+    var nombre = $("#nombre").val();
+    var apPaterno = $("#apPaterno").val();
+    var apMaterno = $("#apMaterno").val();
+    var idDomicilio = 1;
+    var estadocivil = $("#estadocivil").val();
+    var Telefonocasa = $("#Telefonocasa").val();
+    var Telefonocelular = $("#Telefonocelular").val();
+
+    //domicilio
+    //var idColonia
+    var calle = $("#calle").val();
+    var numExt = $("#numExt").val();
+    var numInt = $("#numInt").val();
+    var entrecalle = $("#entrecalle").val();
+    var ycalle = $("#ycalle").val();
+
+    var colonia = $("#colonia").val();//opcional para poner lista desplegable
+    var municipio = $("#municipio").val();//opcional para poner lista desplegable
+
+    //si tiene trabajo llenar datos
+    var empresa = $("#empresa").val();
+    var area = $("#area").val();
+    var telyhora = $("#telyhora").val();//opcion para separarlo en dos campos
+    var idEstudioSocioeconomico
+
+    //ingresos
+    var Ingresopaciente = $("#Ingresopaciente").val();
+    var IngresoPareja = $("#IngresoPareja").val();
+    var IngresoPadres = $("#IngresoPadres").val();
+    var IngresoHijos = $("#IngresoHijos").val();
+    var IngresoOtros = $("#IngresoOtros").val();
+    var sumtotalingreso = parseFloat(Ingresopaciente) +  parseFloat(IngresoPareja) +  parseFloat(IngresoPadres) +  parseFloat(IngresoHijos) +  parseFloat(IngresoOtros);
+    var IngresoTotal = $("#IngresoTotal").val();
+
+    var planta = ($("#sltPlanta").val() == "1");
+    var activo = $("#chkActivo").prop('checked');
 
     //var consultorio = new Object();
     //consultorio.Nombre = nombre;
